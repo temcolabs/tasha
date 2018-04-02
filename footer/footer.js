c64810587971680345ebb69078b750a45dcb54b4
Brian <te>
2018-04-02 15:01:53 -0400

    to support cross browsing on footer

--- footer/footer.js
+++ footer/footer.js
@@ -5,7 +5,11 @@ angular.module('footer', [])
       },
       templateUrl: 'footer/footer.tpl.html',
       link: function($scope) {
-
+      
       }
     };
   });
+//To support image [object-fit: contain] for cross browsing;
+angular.element(document).ready(function () {
+    objectFitImages();
+});
